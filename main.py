878cfd3ffbaad6ccf4171c0127b228916df0d1d7
Scott Rice <meris608@gmail.com>
2013-03-26 17:21:05 -0500

    Added the main file

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+main.py
+
+Created by Scott on 2013-03-26.
+Copyright (c) 2013 Scott Rice. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
